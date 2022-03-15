7f90bddd9754284997c333fbb09277d7e5958552
SatrioBayu <sbayu875@gmail.com>
2022-03-16 01:23:21 +0700

    Menambahkan dokumentasi dan mengubah nama dan struktur function

--- ship.js
+++ ship.js
@@ -1,31 +1,34 @@
-const star = (input) => {
+const ship = (input) => {
   let result = "";
-  //   let row = input * 2;
 
-  // Print Atas
+  // Print Top
   for (let i = 0; i < input; i++) {
+    // Add space to result
     for (let j = 0; j < 3 * input - 2 - i; j++) {
       result += " ";
     }
+    // Add '*' to result
     for (let x = 0; x < 2 * input - (2 * input - 2 * i - 1); x++) {
       result += "*";
     }
     result += "\n";
   }
 
-  // Print Bawah
+  // Print Bottom
   for (let i = 0; i < input; i++) {
+    // Add space to result
     for (let x = 0; x < 2 * i; x++) {
       result += " ";
     }
+    // Add '*' to result
     for (let j = 0; j < 6 * input - 3 - 4 * i; j++) {
       result += "*";
     }
     result += "\n";
   }
-  return result;
+  console.log(result);
 };
-console.log(star(1));
-console.log(star(2));
-console.log(star(3));
-console.log(star(4));
+ship(1);
+ship(2);
+ship(3);
+ship(4);
