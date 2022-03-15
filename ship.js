511ba3aa0227ceaa06a11b1bf2e2eb14bfb64ebe
MalekFazar <fazarmalek@gmail.com>
2022-03-15 22:05:10 +0700

    nambah Looping Atas

--- ship.js
+++ ship.js
@@ -1,1 +1,32 @@
-console.log("hello");
+// Ciee Lagi Liat Kodingan Ini
+
+function ship(key) {
+    let result = '';
+
+    for (let i = 1; i <= key; i++) {
+        for (let j = 1; j <= (key - i); j++) {
+            result += ' ';
+        }
+
+        for (let k = 0; k != (2 * i - 1); k++) {
+            result += '*';
+        }
+
+        for (let l = 1; l <= (key - i); l++) {
+            result += ' ';
+        }
+
+        result += '\n'
+        
+    }
+
+    for (let i = 1; i <= key; i++) {
+        
+        
+    }
+
+    return console.log(result);
+
+}
+
+ship(2)
