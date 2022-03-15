4d16e92cb03a733e3e7615f43042fc0c744ef59c
SatrioBayu <sbayu875@gmail.com>
2022-03-16 01:00:35 +0700

    Menambahkan fungsi star untuk mencetak pattern sesuai tugas

--- ship.js
+++ ship.js
@@ -1,26 +1,31 @@
 const star = (input) => {
   let result = "";
-  let row = input * 2;
-  for (let i = 0; i < row; i++) {
-    // Print bagian atas
-    if (i < input) {
-      // Print space
-      for (let x = 0; x < row - i - 1; x++) {
-        result += " ";
-      }
-      // Print * bagian atas
-      for (let j = 0; j < row - (row - input * i - 1); j++) {
-        result += "*";
-      }
+  //   let row = input * 2;
+
+  // Print Atas
+  for (let i = 0; i < input; i++) {
+    for (let j = 0; j < 3 * input - 2 - i; j++) {
+      result += " ";
     }
-    // Print bagian bawah belum kelar
-    else {
-      for (let y = 0; y < i; y++) {}
+    for (let x = 0; x < 2 * input - (2 * input - 2 * i - 1); x++) {
+      result += "*";
     }
-    if (i != row - 1) {
-      result += "\n";
+    result += "\n";
+  }
+
+  // Print Bawah
+  for (let i = 0; i < input; i++) {
+    for (let x = 0; x < 2 * i; x++) {
+      result += " ";
     }
+    for (let j = 0; j < 6 * input - 3 - 4 * i; j++) {
+      result += "*";
+    }
+    result += "\n";
   }
   return result;
 };
+console.log(star(1));
 console.log(star(2));
+console.log(star(3));
+console.log(star(4));
