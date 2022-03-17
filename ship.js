92c61a765a63c6d29ceacbdecd5f3c63c86ed0a4
L1wa <primayuda86@gmail.com>
2022-03-17 23:16:31 +0700

    membuat segitiga atas ship

--- ship.js
+++ ship.js
@@ -1,32 +1,20 @@
 function ship(number) {
-  // variable penampung *
-  let result = "";
-  // looping bagian atas ship
+  let string = "";
+  // External loop
   for (let i = 1; i <= number; i++) {
-    for (let j = 1; j <= 3 * number - i; j++) {
-      result += " ";
+    // printing spaces
+    for (let j = 1; j <= number - i; j++) {
+      string += " ";
     }
-
-    for (let k = 0; k < i * 2 - 1; k++) {
-      result += "*";
+    // printing star
+    for (let k = 0; k < 2 * i - 1; k++) {
+      string += "*";
     }
-
-    result += "\n";
+    string += "\n";
   }
-  // looping bagian bawah ship
-  for (let i = 1; i <= number; i++) {
-    for (let j = 1; j <= i; j++) {
-      result += " ";
-    }
-
-    for (let k = 0; k < 2 * (3 * number - i) - 1; k++) {
-      result += "*";
-    }
-    result += "\n";
-  }
-  return result;
+  return string;
 }
 console.log(ship(1));
 console.log(ship(2));
-console.log(ship(3));
-console.log(ship(4));
+// console.log(ship(3));
+// console.log(ship(4));
