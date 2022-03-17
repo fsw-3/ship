6d104c84a9e5561f0176ba014ffce42dd1eddcae
MalekFazar <fazarmalek@gmail.com>
2022-03-17 18:46:54 +0700

    Banyak Perubahan Intinya

--- ship.js
+++ ship.js
@@ -1,52 +1,69 @@
 // Ciee Lagi Liat Kodingan Ini
 
 function ship(key) {
+    // Variabel Untuk Menampilkan Kapal
     let result = '';
-    let pow_length = 1;
-    let pow_space = 1;
+    // Variabel Untuk Menyimpan Hasil Pangkat 2
+    let pow = 1;
+    let pow2 = 1;
 
-    for(let i = 1; i <= key; i++){
-        pow_length = pow_length * 3;
+    // Looping Pangkat 2
+    for(let j = 1; j <= key; j++){
+        pow = pow * 2
     }
 
     for(let j = 1; j <= key; j++){
-        pow_space = pow_space * 2
+        pow2 = pow2 * 3
     }
 
+    // Looping Atas
     for (let j = 1; j <= key; j++) {
 
-        // angka 1 disitu harus ada Dynamic, cuman bingun apa yach wkwkwk
-        for (let k = 0; k < (pow_space + 1) - j; k++) {
+        // Looping Pertama Untuk Memberikan Spasi Kepada Bintang
+        for (let k = 0; k < pow - j; k++) {
             result += ' '
         }
 
+        // Looping Kedua Untuk Memberikan Spasi Kepada Bintang
         for (let l = 0; l < (2 * j - 1); l++) {
             result += '*';
         }
 
+        // Memberikan Garis Baru Kepada Setiap Hasil Looping
         result += '\n'
         
     }
 
-    // Looping dibawah udah di tambah, cuman logika nya belum pasti, yang pasti setiap turunan harus dikurang dua
-    for (let o = 1; o <= key; o++) {
-        // for (let o = 0; o < pow; o++) {
-        //     result += ' ';
-        // }
+    // Looping Bawah
+    for (let m = 1; m <= key; m++) {
 
-        for (let p = 0; p < pow_length; p++) {
+        // // Looping Pertama Untuk Memberikan Spasi Kepada Bintang
+        for (let n = 1; n < m ; n++) {
+            result += ' '
+        }
+
+        // Looping Kedua Untuk Memberikan Spasi Kepada Bintang
+        for (let o = 0; o < 2 * ((pow2 - pow) - m) - 1; o++) {
             result += '*';
         }
 
-        // for (let q = 1; q <= (key - n); q++) {
-        //     result += ' ';
-        // }
-
+        // Memberikan Garis Baru Kepada Setiap Hasil Looping
         result += '\n'
     }
 
+    // Untuk Memberikan Hasil Dari Function
     return console.log(result);
 
 }
 
-ship(3)
+// Menampilkan Kapal
+ship(1);
+console.log('\n');
+ship(2);
+console.log('\n');
+ship(3);
+console.log('\n');
+ship(4);
+console.log('\n');
+ship(5);
+console.log('\n');
