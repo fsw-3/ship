ff79338523e8a9a2f22da67658220a47b5f1504b
MalekFazar <fazarmalek@gmail.com>
2022-03-17 17:55:10 +0700

    Looping atas sedang diperbaiki Logic nya dan Looping bawah sudah di buat tinggal Logic nya

--- ship.js
+++ ship.js
@@ -2,31 +2,51 @@
 
 function ship(key) {
     let result = '';
+    let pow_length = 1;
+    let pow_space = 1;
 
-    for (let i = 1; i <= key; i++) {
-        for (let j = 1; j <= (key - i); j++) {
-            result += ' ';
+    for(let i = 1; i <= key; i++){
+        pow_length = pow_length * 3;
+    }
+
+    for(let j = 1; j <= key; j++){
+        pow_space = pow_space * 2
+    }
+
+    for (let j = 1; j <= key; j++) {
+
+        // angka 1 disitu harus ada Dynamic, cuman bingun apa yach wkwkwk
+        for (let k = 0; k < (pow_space + 1) - j; k++) {
+            result += ' '
         }
 
-        for (let k = 0; k != (2 * i - 1); k++) {
+        for (let l = 0; l < (2 * j - 1); l++) {
             result += '*';
-        }
-
-        for (let l = 1; l <= (key - i); l++) {
-            result += ' ';
         }
 
         result += '\n'
         
     }
 
-    for (let i = 1; i <= key; i++) {
-        
-        
+    // Looping dibawah udah di tambah, cuman logika nya belum pasti, yang pasti setiap turunan harus dikurang dua
+    for (let o = 1; o <= key; o++) {
+        // for (let o = 0; o < pow; o++) {
+        //     result += ' ';
+        // }
+
+        for (let p = 0; p < pow_length; p++) {
+            result += '*';
+        }
+
+        // for (let q = 1; q <= (key - n); q++) {
+        //     result += ' ';
+        // }
+
+        result += '\n'
     }
 
     return console.log(result);
 
 }
 
-ship(2)
+ship(3)
