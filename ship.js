06748d37ef83bbb500752756fd1bcf3feba57fcd
bobbyrizkinh15 <bobbyrizki15@gmail.com>
2022-03-17 17:01:29 +0700

    membuat segitiga samakaki

--- ship.js
+++ ship.js
@@ -1,1 +1,15 @@
-console.log("bobbycd");
+let n = 5;
+let hasil = "";
+// External looping
+for (let i = 1; i <= n; i++) {
+    // cetak spasi
+    for (let j = 1; j <= n - i; j++) {
+        hasil += " ";
+    }
+    // cetak bintang
+    for (let k = 0; k < 2 * i - 1; k++) {
+        hasil += "*";
+    }
+    hasil += "\n";
+}
+console.log(hasil);
