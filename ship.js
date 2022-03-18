128de8adeba3a4c2bbf4ca0135b77b9f74efc314
Ali Fahrial Anwar <alifahrialanwar@gmail.com>
2022-03-18 17:28:41 +0700

    'update'

--- ship.js
+++ ship.js
@@ -1,20 +1,3 @@
-// function shipStar(star){
-//     let result = '';
-
-//     //loping top
-//     for (let i = 1; i <= star + 1; i++) {
-//         //space loop
-//         for (let s = 1; s <= 2 * star + 1 - i; s++) {
-//             result += ' ';
-//         }
-//         //stars loop top
-//         for (let j = 0; j <= 2 * i - 2 ; j++) {
-//             result += '*';
-//         }
-//         result += '\n';
-//     }
-//     return result;
-// }
 function shipStar(star){
     let result = '';
 
