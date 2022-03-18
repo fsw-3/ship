6d3898c016ed6c00c1308311ff4a8ab169150a14
Ali Fahrial Anwar <alifahrialanwar@gmail.com>
2022-03-18 13:51:35 +0700

    looping top

--- ship.js
+++ ship.js
@@ -1,1 +1,18 @@
-console.log('Hello !');
+function shipStar(star){
+    let result = '';
+
+    //loping top
+    for (let i = 1; i <= star + 1; i++) {
+        //space loop
+        for (let s = 1; s <= 3 * star - i; s++) {
+            result += ' ';
+        }
+        //stars loop top
+        for (let j = 1; j < 3 * i - 1 ; j++) {
+            result += '*';
+        }
+        result += '\n';
+    }
+    return result;
+}
+console.log(shipStar(1));
