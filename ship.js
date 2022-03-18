3bb34758103b3e19acc11e681dda22814a4713ed
L1wa <primayuda86@gmail.com>
2022-03-18 20:05:37 +0700

    looping ship

--- ship.js
+++ ship.js
@@ -3,23 +3,11 @@ function ship(star) {
   //   looping bagian ats ship
   for (let i = 0; i < star; i++) {
     // printing spaces
-    for (j = 0; j < 3 * star - 2 - i; j++) {
+    for (let j = 0; j <= star - i; j++) {
       string += " ";
     }
     // printing star
-    for (let k = 0; k < 1 + 2 * i; k++) {
-      string += "*";
-    }
-    string += "\n";
-  }
-  //   looping bagian bawah ship
-  for (let i = 0; i < star; i++) {
-    // printing spaces
-    for (let j = 0; j < 2 * i; j++) {
-      string += " ";
-    }
-    // printing star
-    for (let k = 0; k < 6 * star - 3 - 4 * i; k++) {
+    for (let k = 0; k < 2 * i - 1; k++) {
       string += "*";
     }
     string += "\n";
