b72013e778014ed5a3cf87170fbce4468f30eb72
triyasniko <triyasniko@gmail.com>
2022-03-18 17:22:10 +0700

    baru segitiga pyramid biasa

--- ship.js
+++ ship.js
@@ -1,1 +1,22 @@
+function ship(number){
+    let result='';
 
+    // jumlah baris
+    for(let i=1;i<=number;i++){
+        // spasi
+        for (let j=i;j<number;j++){
+            result+=' ';
+        }
+        for (let k=1;k<=i;k++){
+            result +='*';
+        }
+        for (let l=1;l<=i-1;l++){
+            result +='*';
+        }
+        result+='\n';
+    }
+
+    return result;
+
+}
+console.log(ship(2));
